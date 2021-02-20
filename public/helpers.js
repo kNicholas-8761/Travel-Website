22ce3c64cffbfb28f2aa982c377d707aad37e857
user <kamauri84@gmail.com>
2021-02-20 22:43:06 +1100

    create venue function

---
+++ public/helpers.js
@@ -0,0 +1,8 @@
+const createVenueHTML = (name, location, iconSource) => {
+    return `<h2>${name}</h2>
+    <img class="venueimage" src="${iconSource}"/>
+    <h3>Address:</h3>
+    <p>${location.address}</p>
+    <p>${location.city}</p>
+    <p>${location.country}</p>`;
+  }
