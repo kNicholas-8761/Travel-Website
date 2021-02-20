7dd047c2642d4f1ac7950a91a4e95bdae9f45ab5
user <kamauri84@gmail.com>
2021-02-21 01:04:09 +1100

    add api credentials

--- public/main.js
+++ public/main.js
@@ -1,8 +1,59 @@
 // Foursquare API Info
-const clientId = '';
-const clientSecret = '';
-const url = '';
+const clientId = 'ZUQT4GLKGABQVEYH53PB0NO5VJXP4DXCWMGDNTZZHO3LNWT1';
+const clientSecret =' HQEFIZ113VS35OOZVFL0SQYDKBDWP4LAPJPKJOJWWPJUPVPP'
+const url = 'https://api.foursquare.com/v2/venues/explore?near=';
 
 // OpenWeather Info
-const openWeatherKey = '';
-const weatherUrl = '';
+const openWeatherKey = 'f8f3fa023d23a1b9d80516b165c59df2';
+const weatherUrl =  'https://api.openweathermap.org/data/2.5/weather';
+
+// Page Elements
+const $input = $('#city');
+const $submit = $('#button');
+const $destination = $('#destination');
+const $container = $('.container');
+const $venueDivs = [$("#venue1"), $("#venue2"), $("#venue3"), $("#venue4")];
+const $weatherDiv = $("#weather1");
+const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+
+
+
+const getVenues = () => {
+
+}
+
+const getForecast = () => {
+
+}
+
+
+// Render functions
+const renderVenues = (venues) => {
+  $venueDivs.forEach(($venue, index) => {
+    // Add your code here:
+
+    let venueContent = '';
+    $venue.append(venueContent);
+  });
+  $destination.append(`<h2>${venues[0].location.city}</h2>`);
+}
+
+const renderForecast = (day) => {
+  
+  
+	let weatherContent = '';
+  $weatherDiv.append(weatherContent);
+}
+
+const executeSearch = () => {
+  $venueDivs.forEach(venue => venue.empty());
+  $weatherDiv.empty();
+  $destination.empty();
+  $container.css("visibility", "visible");
+  getVenues()
+  getForecast()
+  return false;
+}
+
+$submit.click(executeSearch)
