18c0517020ea85701cd010a06714f8aa9b2b78b4
user <kamauri84@gmail.com>
2021-02-20 22:45:06 +1100

    create weather function

--- public/helpers.js
+++ public/helpers.js
@@ -6,3 +6,11 @@ const createVenueHTML = (name, location, iconSource) => {
     <p>${location.city}</p>
     <p>${location.country}</p>`;
   }
+
+  const createWeatherHTML = (currentDay) => {
+    console.log(currentDay)
+    return `<h2>${weekDays[(new Date()).getDay()]}</h2>
+          <h2>Temperature: ${kelvinToFahrenheit(currentDay.main.temp)}&deg;F</h2>
+          <h2>Condition: ${currentDay.weather[0].description}</h2>
+        <img src="https://openweathermap.org/img/wn/${currentDay.weather[0].icon}@2x.png">`;
+  }
