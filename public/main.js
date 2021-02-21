64a326ca3329662626dc58107fe17f4c17265319
user <kamauri84@gmail.com>
2021-02-21 11:10:46 +1100

    create try catch block and log error message.

--- public/main.js
+++ public/main.js
@@ -22,6 +22,11 @@ const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Frida
 const getVenues = async () => {
   const city = $input.val();
   const urlToFetch = `${url}${city}&limit=10&client_id =${clientId}&client_secret=${clientSecret}&v=20210221`;
+  try {
+    
+  } catch (error) {
+    console.log(error);
+  }
 
 }
 
